785b05f21ff1d0d27fe1abddc528abaee0375467
Volgr1 <147431815+Volgr1@users.noreply.github.com>
2026-05-14 20:49:51 +0300

    Add engines and port config

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
 // Замени на свой URL от MockAPI
 const MOCKAPI_BASE_URL = 'https://6a05c29caa826ca75c0a8930.mockapi.io';
 
